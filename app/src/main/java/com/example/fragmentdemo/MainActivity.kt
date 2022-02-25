6948e6fefe59f43273a876fd1bc30db52fc0ae6f
Zheng240901 <tongzh-wm19@student.tarc.edu.my>
2022-02-25 22:21:22 +0800

    First commit

---
+++ app/src/main/java/com/example/fragmentdemo/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.fragmentdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.fragmentdemo.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val nav by lazy { supportFragmentManager.findFragmentById(R.id.host)!!.findNavController() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // TODO: Setup action bar
+        setupActionBarWithNavController(nav)
+    }
+
+    // TODO: onSupportNavigateUp()
+
+    override fun onSupportNavigateUp(): Boolean {
+        return nav.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    // TODO: onCreateOptionsMenu()
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // TODO: onOptionsItemSelected()
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.hello->{
+                Snackbar.make(binding.root, "Hello", Snackbar.LENGTH_SHORT).show()
+                return true
+            }
+        }
+
+        //clear backstack
+        return item.onNavDestinationSelected(nav) || super.onOptionsItemSelected(item)
+    }
+
+}
